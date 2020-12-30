35d43c7bf8b2d8ec7e71211fab09202061fd3f4e
shahala-hakkim <76463953+shahala-hakkim@users.noreply.github.com>
2020-12-30 14:30:43 +0530

    Add files via upload

---
+++ encapsulation/Student.java
@@ -0,0 +1,38 @@
+package comm;
+
+public class Student {
+	private int rollno,marks;
+	private String name,collegeName;
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public int getMarks() {
+		return marks;
+	}
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCollegeName() {
+		return collegeName;
+	}
+	public void setCollegeName(String collegeName) {
+		this.collegeName = collegeName;
+	}
+	@Override
+	public String toString() {
+		return "Student [rollno=" + rollno + ", marks=" + marks + ", name=" + name + ", collegeName=" + collegeName
+				+ "]";
+	}
+	
+	
+
+}
